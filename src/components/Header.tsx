40cc229c11cf111ec49c68a3ac017e30c7840c3c
ichio kon <ichiokondo007@proton.me>
2025-02-15 03:39:01 +0900

    tailwindエラー修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import Image from 'next/image';
-
 export default function Header() {
     return (
         <header className="h-[75px] bg-gray-900 text-white fixed top-0 w-full flex items-center justify-between px-4">
             {/* ロゴ */}
             <div className="flex">
-                <Image src="/img/kd_logo114.png" alt="KDLabs" width={65} height={65} className="h-[65px] w-auto" />
+                <img src="/img/kd_logo114.png" alt="KDLabs" className="h-[65px] w-auto" />
             </div>
             {/* メニュー */}
             <nav className="flex gap-5">
@@ -19,7 +17,7 @@ export default function Header() {
             </nav>
             {/* ユーザーアイコン */}
             <div className="flex">
-                <Image src="/img/user.svg" alt="user" width={60} height={60} className="h-[60px] w-auto invert" />
+                <img src="/img/user.svg" alt="user" className="h-[60px] w-auto invert" />
             </div>
         </header>
     );
